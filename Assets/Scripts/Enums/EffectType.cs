c169185b1f643e6782703d6dcb8aeddbaa0c6c0b
David Novaković <daki.nole@gmail.com>
2026-04-23 02:13:37 +0200

    Added status effect type enum

---
+++ Assets/Scripts/Enums/EffectType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NordeusRPG.Enums
+{
+    public enum EffectType
+    {
+        BuffAttack,
+        BuffDefense,
+        DebufAttack,
+        DebufDefense,
+        Poison,
+        HealOverTime
+    }
+}

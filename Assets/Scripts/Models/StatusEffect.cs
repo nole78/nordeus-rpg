d9e856b814e24c96db5199ba49e3e27fa5678fde
David Novaković <daki.nole@gmail.com>
2026-04-23 02:06:34 +0200

    Added Status Effect model

---
+++ Assets/Scripts/Models/StatusEffect.cs
@@ -0,0 +1,10 @@
+﻿
+namespace NordeusRPG.Models
+{
+    public class StatusEffect
+    {
+        public string Stat { get; set; } = string.Empty;
+        public int Value { get; set; } = 0;
+        public int RemainingTurns { get; set; } = 0;
+    }
+}

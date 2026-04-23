7c1c188af39e5d28f2de5e6c6ab0d5212fd3b436
David Novaković <daki.nole@gmail.com>
2026-04-23 02:06:15 +0200

    Added character model

--- Assets/Scripts/Models/Character.cs
+++ Assets/Scripts/Models/Character.cs
@@ -1,18 +1,16 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Character : MonoBehaviour
+namespace NordeusRPG.Models
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Character
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public string Name { get; set; } = string.Empty;
+        public int MaxHealth { get; set; } = 100;
+        public int CurrentHealth { get; set; } = 100;
+        public int Attack { get; set; } = 1;
+        public int Defense { get; set; } = 1;
+        public int Magic { get; set; } = 1;
+        public List<Move> Moves { get; set; } = new List<Move>();
     }
 }

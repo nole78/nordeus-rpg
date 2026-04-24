8c0ea7213b6956a189fbb82223f434f18b5f95ee
David Novaković <daki.nole@gmail.com>
2026-04-24 03:30:23 +0200

    Improved Character model by making Health a default!

--- TurnBasedRPG.API/Models/Character.cs
+++ TurnBasedRPG.API/Models/Character.cs
@@ -3,9 +3,9 @@
     public class Character
     {
         public string Name { get; set; } = string.Empty;
-        public Health Health { get; set; } = new Health(100);
-        public Stats BaseStats { get; set; } = new Stats();
-        public List<Move> Moves { get; set; } = new List<Move>();
-        public List<StatusEffect> StatusEffects { get; set; } = new List<StatusEffect>();
+        public Health Health { get; set; } = default!;
+        public Stats BaseStats { get; set; } = new();
+        public List<Move> Moves { get; set; } = [];
+        public List<StatusEffect> StatusEffects { get; set; } = [];
     }
 }

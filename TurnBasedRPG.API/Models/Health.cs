41e41594bd78adb625b07e302f5c5ff2c9237154
David Novaković <daki.nole@gmail.com>
2026-04-24 02:42:44 +0200

    Added health model to server

---
+++ TurnBasedRPG.API/Models/Health.cs
@@ -0,0 +1,37 @@
+﻿namespace TurnBasedRPG.API.Models
+{
+    public class Health
+    {
+        public int MaxHealth { get; private set; } = 0;
+        public int CurrentHealth { get; private set; } = 0;
+
+        public Health(int maxHealth)
+        {
+            MaxHealth = maxHealth;
+            CurrentHealth = maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            CurrentHealth -= damage;
+            if (CurrentHealth < 0)
+            {
+                CurrentHealth = 0;
+            }
+        }
+
+        public void Heal(int healAmount)
+        {
+            CurrentHealth += healAmount;
+            if (CurrentHealth > MaxHealth)
+            {
+                CurrentHealth = MaxHealth;
+            }
+        }
+
+        public bool isDead()
+        {
+            return CurrentHealth <= 0;
+        }
+    }
+}

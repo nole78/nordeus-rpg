a4a5fe420bf06d5186e22a9813c120a15bd1b4ee
David Novaković <daki.nole@gmail.com>
2026-04-24 03:34:16 +0200

    Added DTO for next move request

---
+++ TurnBasedRPG.API/DTOs/NextMoveRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TurnBasedRPG.API.Models;
+
+namespace TurnBasedRPG.API.DTOs
+{
+    public class NextMoveRequest
+    {
+        [Required]
+        public BattleState CurrentState { get; set; } = default!;
+        [Required(AllowEmptyStrings = false)]
+        public string PlayerMove { get; set; } = string.Empty;
+    }
+}

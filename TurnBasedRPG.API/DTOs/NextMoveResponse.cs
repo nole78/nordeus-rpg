9a888fbbde677372dd6d2e84e73235846f659e79
David Novaković <daki.nole@gmail.com>
2026-04-24 03:34:31 +0200

    Added NextMoveResposnse DTO

---
+++ TurnBasedRPG.API/DTOs/NextMoveResponse.cs
@@ -0,0 +1,9 @@
+﻿using TurnBasedRPG.API.Models;
+
+namespace TurnBasedRPG.API.DTOs
+{
+    public class NextMoveResponse
+    {
+        public required BattleState UpdatedState { get; set; }
+    }
+}

2344b4e712f933e735e4e4c8c9e505e1851f8b4b
David Novaković <daki.nole@gmail.com>
2026-04-24 02:43:04 +0200

    Added stats model to server

---
+++ TurnBasedRPG.API/Models/Stats.cs
@@ -0,0 +1,9 @@
+﻿namespace TurnBasedRPG.API.Models
+{
+    public class Stats
+    {
+        public int Attack { get; set; } = 0;
+        public int Defense { get; set; } = 0;
+        public int Magic { get; set; } = 0;
+    }
+}

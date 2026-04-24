53c12572efcc0b54209b4dfc813c993168954048
David Novaković <daki.nole@gmail.com>
2026-04-24 02:41:50 +0200

    Added EffectType enum in server model

---
+++ TurnBasedRPG.API/Enums/EffectType.cs
@@ -0,0 +1,11 @@
+﻿namespace TurnBasedRPG.API.Enums
+{
+    public enum EffectType
+    {
+        BuffAttack,
+        BuffDefense,
+        DebuffAttack,
+        DebuffDefense,
+        Poison
+    }
+}

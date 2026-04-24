c0b384b8f3a4adabaab2898f5693361a6b2d8b1a
David Novaković <daki.nole@gmail.com>
2026-04-24 02:42:31 +0200

    Added character model on server

---
+++ TurnBasedRPG.API/Models/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace TurnBasedRPG.API.Models
+{
+    public class Character
+    {
+        public string Name { get; set; } = string.Empty;
+        public Health Health { get; set; } = new Health(100);
+        public Stats BaseStats { get; set; } = new Stats();
+        public List<Move> Moves { get; set; } = new List<Move>();
+        public List<StatusEffect> StatusEffects { get; set; } = new List<StatusEffect>();
+    }
+}

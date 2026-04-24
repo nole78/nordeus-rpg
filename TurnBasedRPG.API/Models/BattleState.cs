2a10b367133e990e5f25ebe0c3a8e470f4f37774
David Novaković <daki.nole@gmail.com>
2026-04-24 02:42:18 +0200

    Added battle state model on server

---
+++ TurnBasedRPG.API/Models/BattleState.cs
@@ -0,0 +1,9 @@
+﻿namespace TurnBasedRPG.API.Models
+{
+    public class BattleState
+    {
+        public Character Hero { get; set; }
+        public Character Enemy { get; set; }
+        public int CurrentTurn { get; set; } = 1;
+    }
+}

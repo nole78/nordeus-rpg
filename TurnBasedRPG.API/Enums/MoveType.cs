e831599982f22097ad4f9f40d0ca37cd27d919c5
David Novaković <daki.nole@gmail.com>
2026-04-24 02:42:04 +0200

    Added MoveType enum in client model

---
+++ TurnBasedRPG.API/Enums/MoveType.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.NetworkInformation;
+
+namespace TurnBasedRPG.API.Enums
+{
+    public enum MoveType
+    {
+        Physical,
+        Magic,
+        Buff,
+        Debuff,
+        Heal
+    }
+}

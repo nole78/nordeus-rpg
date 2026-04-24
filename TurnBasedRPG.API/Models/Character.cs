528f9c5ffd251cb8326ee3a9499d30439576010e
David Novaković <daki.nole@gmail.com>
2026-04-24 03:32:33 +0200

    Improved Character by making Health and Stats required

--- TurnBasedRPG.API/Models/Character.cs
+++ TurnBasedRPG.API/Models/Character.cs
@@ -3,8 +3,8 @@
     public class Character
     {
         public string Name { get; set; } = string.Empty;
-        public Health Health { get; set; } = default!;
-        public Stats BaseStats { get; set; } = new();
+        public required Health Health { get; set; }
+        public required Stats BaseStats { get; set; }
         public List<Move> Moves { get; set; } = [];
         public List<StatusEffect> StatusEffects { get; set; } = [];
     }

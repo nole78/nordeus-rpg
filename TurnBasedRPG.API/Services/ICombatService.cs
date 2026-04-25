06b1ca0e05a7b87d490b10d09d94bf54ef01adb2
David Novaković <daki.nole@gmail.com>
2026-04-25 02:23:22 +0200

    Added CombatService interface and implemntation

---
+++ TurnBasedRPG.API/Services/ICombatService.cs
@@ -0,0 +1,9 @@
+﻿using TurnBasedRPG.API.DTOs;
+
+namespace TurnBasedRPG.API.Services
+{
+    public interface ICombatService
+    {
+        NextMoveResponse ProcessTurn(NextMoveRequest request);
+    }
+}

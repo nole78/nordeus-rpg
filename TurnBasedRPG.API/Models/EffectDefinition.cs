11bd4260f84770d50fdc225e731ad5a6116941ff
David Novaković <daki.nole@gmail.com>
2026-04-25 13:54:14 +0200

    Added EffectDefinition model

---
+++ TurnBasedRPG.API/Models/EffectDefinition.cs
@@ -0,0 +1,11 @@
+﻿using TurnBasedRPG.API.Enums;
+
+namespace TurnBasedRPG.API.Models
+{
+    public class EffectDefinition
+    {
+        public EffectType Type { get; set; }
+        public int Value { get; set; }
+        public int Duration { get; set; }
+    }
+}

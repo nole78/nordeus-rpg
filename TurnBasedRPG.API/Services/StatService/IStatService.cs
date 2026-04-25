895245be39e2a537e0b921903110918e9a9c228e
David Novaković <daki.nole@gmail.com>
2026-04-26 01:13:12 +0200

    Added StatService interface and implementation

---
+++ TurnBasedRPG.API/Services/StatService/IStatService.cs
@@ -0,0 +1,9 @@
+﻿using TurnBasedRPG.API.Models;
+
+namespace TurnBasedRPG.API.Services.StatService
+{
+    public interface IStatService
+    {
+        (int attack,int magic,int deffense) CalculateStats(Character attacker, Character defender);
+    }
+}
---
+++ TurnBasedRPG.API/Services/StatService/StatService.cs
@@ -0,0 +1,63 @@
+﻿using TurnBasedRPG.API.Enums;
+using TurnBasedRPG.API.Models;
+
+namespace TurnBasedRPG.API.Services.StatService
+{
+    public class StatService : IStatService
+    {
+        public (int attack, int magic, int deffense) CalculateStats(Character attacker, Character defender)
+        {
+            int attack = attacker.BaseStats.Attack;
+            int magic = attacker.BaseStats.Magic;
+            int defense = defender.BaseStats.Defense;
+
+            foreach (var effect in attacker.StatusEffects)
+            {
+                switch (effect.Type)
+                {
+                    case EffectType.BuffAttack:
+                        {
+                            attack += effect.Value;
+                        }
+                        ; break;
+                    case EffectType.BuffMagic:
+                        {
+                            magic += effect.Value;
+                        }
+                        ; break;
+                    case EffectType.DebuffAttack:
+                        {
+                            attack -= effect.Value;
+                        }
+                        ; break;
+                    case EffectType.DebuffMagic:
+                        {
+                            magic -= effect.Value;
+                        }
+                        ; break;
+                }
+            }
+            foreach (var effect in defender.StatusEffects)
+            {
+                switch (effect.Type)
+                {
+                    case EffectType.BuffDefense:
+                        {
+                            defense += effect.Value;
+                        }
+                        ; break;
+                    case EffectType.DebuffDefense:
+                        {
+                            defense -= effect.Value;
+                        }
+                        ; break;
+                }
+            }
+            attack = Math.Max(0, attack);
+            magic = Math.Max(0, magic);
+            defense = Math.Max(0, defense);
+
+            return (attack, magic, defense);
+        }
+    }
+}

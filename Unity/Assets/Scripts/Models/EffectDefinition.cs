956f04c78069290b2e40510487d00f5a519951b2
David Novaković <daki.nole@gmail.com>
2026-04-27 17:51:15 +0200

    Updated models to match server ones

---
+++ Unity/Assets/Scripts/Models/EffectDefinition.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Enums;
+using NordeusRPG.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Models
+{
+    public class EffectDefinition
+    {
+        public EffectType Type { get; set; } = EffectType.BuffAttack;
+        public int Value { get; set; } = 0;
+        public int Duration { get; set; } = 0;
+        public StackingRule Stacking { get; set; } = StackingRule.Replace;
+    }
+}
---
+++ Unity/Assets/Scripts/Models/MoveEffect.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Models
+{
+    public class MoveEffect
+    {
+        public MoveKind Kind { get; set; } = MoveKind.Damage;
+        public TargetType Target { get; set; } = TargetType.Enemy;
+        public int Value { get; set; } = 0;
+        public StatType ScalingStat { get; set; } = StatType.None;
+        public EffectDefinition? StatusEffect { get; set; }
+    }
+}

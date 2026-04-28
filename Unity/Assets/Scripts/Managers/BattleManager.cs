5d244c1f92898d4ea90b93942399d1fddcd55fe5
David Novaković <daki.nole@gmail.com>
2026-04-29 00:04:17 +0200

    Implemented BattleManager as an Singleton (not global) that holds batlestate and calls api

--- Unity/Assets/Scripts/Managers/BattleManager.cs
+++ Unity/Assets/Scripts/Managers/BattleManager.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+using NordeusRPG.DTOs;
 using NordeusRPG.Models;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,17 +7,45 @@ using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
-    private Character hero;
-    private Character enemy;
-    void Start()
+    public static BattleManager Instance;
+    private BattleState _state;
+
+    private void Awake()
     {
-        hero = GameManager.Instance.Hero;
-        enemy = GameManager.Instance.CurrentEnemy;
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Init(Character hero,Character enemy)
     {
-        
+        _state = new BattleState
+        {
+            Hero = hero,
+            Enemy = enemy,
+            CurrentTurn = 1
+        };
+    }
+
+    public void PlayMove(string moveId)
+    {
+        var request = new NextMoveRequest
+        {
+            PlayerMove = moveId,
+            CurrentState = _state
+        };
+
+        StartCoroutine(ApiClient.Instance.SendNextMove(request, OnSucces, OnError));
+    }
+
+    private void OnSucces(NextMoveResponse response)
+    {
+        _state = response.UpdatedState;
+        Debug.Log(JsonConvert.SerializeObject(response));
+        Debug.Log(_state.Enemy.Health.CurrentHealth);
+        FindObjectOfType<BattleUI>().Refresh(_state);
+    }
+
+    private void OnError(string err)
+    {
+        Debug.LogError(err);
     }
 }

03390b133f602335c364684a5e1290e7af531862
David Novaković <daki.nole@gmail.com>
2026-04-28 21:40:44 +0200

    Implemented healthbar logic thorugh HeathBarSlider script and added healthbars initialisation to BattleUI script

--- Unity/Assets/Scripts/UI/BattleUI.cs
+++ Unity/Assets/Scripts/UI/BattleUI.cs
@@ -1,4 +1,6 @@
+using Assets.Scripts.UI;
 using NordeusRPG.DTOs;
+using NordeusRPG.Models;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +8,20 @@ using UnityEngine;
 public class BattleUI : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private Character _hero;
+    private Character _enemy;
+    public HealthBarSlider heroHealthbar;
+    public HealthBarSlider enemyHealthbar;
+    void Start() 
     {
+        _hero = GameManager.Instance.Hero;
+        _enemy = GameManager.Instance.CurrentEnemy;
 
+        heroHealthbar.SetMaxHealth(_hero.Health.MaxHealth);
+        heroHealthbar.SetHealth(_hero.Health.CurrentHealth);
+
+        enemyHealthbar.SetMaxHealth(_enemy.Health.MaxHealth);
+        enemyHealthbar.SetHealth(_enemy.Health.CurrentHealth);
     }
 
     // Update is called once per frame
---
+++ Unity/Assets/Scripts/UI/HealthBarSlider.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class HealthBarSlider : MonoBehaviour
+    {
+        public Slider slider;
+        public TextMeshProUGUI healthText;
+
+        public void SetMaxHealth(int max)
+        {
+            slider.maxValue = max;
+            slider.value = max;
+            SetText(max, max);
+        }
+
+        public void SetHealth(int current)
+        {
+            slider.value = current;
+            SetText(current, (int)slider.maxValue);
+        }
+
+        public void SetNormalized(float percent)
+        {
+            slider.normalizedValue = percent;
+            SetText((int)slider.value, (int)slider.maxValue);
+        }
+
+        public void SetText(int current, int max)
+        {
+            healthText.text = $"{current} / {max}";
+        }
+
+        public void SetHealthSmooth(int target)
+        {
+            StartCoroutine(Smooth(target));
+            SetText(target, (int)slider.maxValue);
+        }
+
+        IEnumerator Smooth(int target)
+        {
+            float start = slider.value;
+            float time = 0;
+
+            while (time < 0.2f)
+            {
+                time += Time.deltaTime;
+                slider.value = Mathf.Lerp(start, target, time / 0.2f);
+                yield return null;
+            }
+
+            slider.value = target;
+        }
+    }
+}

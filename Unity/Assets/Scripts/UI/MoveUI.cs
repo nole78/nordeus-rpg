0740458ed19f943f672a5088b28562d426b1a5ac
David Novaković <daki.nole@gmail.com>
2026-04-28 21:48:02 +0200

    Added move button prefab with its script MoveUI

---
+++ Unity/Assets/Scripts/UI/MoveUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class MoveUI : MonoBehaviour
+    {
+        public Button button;
+        public TextMeshProUGUI text;
+
+        public void Init(Action onClick,string name)
+        {
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() => onClick());
+            text.text = name;
+        }
+
+        public void SetInteractable(bool value)
+        {
+            button.interactable = value;
+        }
+    }
+}

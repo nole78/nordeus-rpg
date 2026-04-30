505a39dc2a8268789160c09b6b81f1e0cc0e3bfb
David Novaković <daki.nole@gmail.com>
2026-04-30 11:54:53 +0200

    Added CharacterVisual database

---
+++ Unity/Assets/Scripts/Database/CharacterVisualDatabase.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.Models;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Database
+{
+    [CreateAssetMenu(menuName = "Game/Character Visuals")]
+    public class CharacterVisualDatabase : ScriptableObject
+    {
+        public List<CharacterVisual> characters;
+        private Dictionary<string, CharacterVisual> _map;
+
+
+        public void Init()
+        {
+            _map = characters.ToDictionary(c => c.Id, c => c);
+        }
+
+        public CharacterVisual GetSprite(string id)
+        {
+            return _map.TryGetValue(id, out var visual) ? visual : null;
+        }
+    }
+}

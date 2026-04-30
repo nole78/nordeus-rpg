d24b8b8aaa022b0efab05d88559d4e38f136042d
David Novaković <daki.nole@gmail.com>
2026-04-30 10:25:29 +0200

    updated MoveUI to now have icon (Image) and iconDb (MoveIconDatabase) public fields.

--- Unity/Assets/Scripts/UI/MoveUI.cs
+++ Unity/Assets/Scripts/UI/MoveUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.Scripts.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,8 @@ namespace Assets.Scripts.UI
     {
         public Button button;
         public TextMeshProUGUI text;
+        public Image icon;
+        public MoveIconDatabase iconDb;
         public string moveId;
 
         public void Init(Action onClick,string name,string id)
@@ -21,6 +24,8 @@ namespace Assets.Scripts.UI
             button.onClick.AddListener(() => onClick());
             text.text = name;
             moveId = id;
+
+            icon.sprite = iconDb.GetSprite(id);
         }
 
         public void SetInteractable(bool value)

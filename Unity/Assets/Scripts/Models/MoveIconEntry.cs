02788aa922533da8cfe2dd7354a9c12595a14081
David Novaković <daki.nole@gmail.com>
2026-04-30 10:24:41 +0200

    Added MoveIconDatabase Scriptable Object, a dtabase of model MoveIconEntry (new model)

---
+++ Unity/Assets/Scripts/Models/MoveIconEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Database
+{
+    [Serializable]
+    public class MoveIconEntry
+    {
+        public string moveId;
+        public Sprite sprite;
+    }
+}

b3c447ca2548fbeeb896905d7a8ddca9e1116c8a
David Novaković <daki.nole@gmail.com>
2026-04-30 21:01:35 +0200

    Updated server side to return event log

--- TurnBasedRPG.API/DTOs/NextMoveResponse.cs
+++ TurnBasedRPG.API/DTOs/NextMoveResponse.cs
@@ -5,5 +5,6 @@ namespace TurnBasedRPG.API.DTOs
     public class NextMoveResponse
     {
         public required BattleState UpdatedState { get; set; }
+        public required List<CombatEvent> Events { get; set; }
     }
 }
---
+++ TurnBasedRPG.API/Domain/Models/CombatEvent.cs
@@ -0,0 +1,16 @@
+﻿using TurnBasedRPG.API.Domain.Enums;
+
+namespace TurnBasedRPG.API.Domain.Models
+{
+    public class CombatEvent
+    {
+        public string AttackerId { get; set; } = string.Empty;
+        public string TargetId { get; set; } = string.Empty;
+
+        public MoveKind Kind { get; set; } = MoveKind.Damage;
+
+        public int Value { get; set; } = 0;
+
+        public bool IsSelf { get; set; } = false;
+    }
+}

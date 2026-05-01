65e0b599598c4dd04b9c07b1c876d6796ad137ef
David Novaković <daki.nole@gmail.com>
2026-05-02 00:20:40 +0200

    Added Effect icon sprite database and Effect prefab with its logic EffectUI.cs

---
+++ Unity/Assets/Scripts/Models/EffectIconEntry.cs
@@ -0,0 +1,13 @@
+﻿using NordeusRPG.Enums;
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Models
+{
+    [Serializable]
+    public class EffectIconEntry
+    {
+        public EffectType type;
+        public Sprite sprite;
+    }
+}
---
+++ Unity/Assets/Scripts/UI/EffectUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class EffectUI : MonoBehaviour
+    {
+        public TextMeshProUGUI duration;
+        public Image image;
+        public void SetVisibility(bool visible)
+        {
+            gameObject.SetActive(visible);
+        }
+        public void SetSprite(Sprite sprite)
+        {
+            image.sprite = sprite;
+        }
+        public void SetDuration(int duration)
+        {
+            this.duration.text = duration.ToString();
+        }
+    }
+}

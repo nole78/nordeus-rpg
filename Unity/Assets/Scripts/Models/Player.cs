6950a7061a25637b683f7380bf3a7cb88e69df7e
David Novaković <daki.nole@gmail.com>
2026-05-02 18:26:09 +0200

    Added wraper class Player for hero character, contains list of all learnt moves, experience and level (so that later selected moves can be changed)

---
+++ Unity/Assets/Scripts/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using NordeusRPG.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Models
+{
+    public class Player
+    {
+        public string Username { get; set; } = string.Empty;
+        public Character Hero { get; set; }
+        public int Level { get; set; } = 1;
+        public int Experience { get; set; } = 0;
+        public List<Move> Moves { get; set; } = new List<Move>();
+    }
+}

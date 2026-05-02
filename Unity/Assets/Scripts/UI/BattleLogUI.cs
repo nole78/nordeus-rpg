4ee3c26646bfed60d46dd8ddb8e8e9d15c9e9766
David Novaković <daki.nole@gmail.com>
2026-05-03 01:29:56 +0200

    Added battle log to battle scene

---
+++ Unity/Assets/Scripts/UI/BattleLogUI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class BattleLogUI : MonoBehaviour
+    {
+        public Transform content;
+        public LogItemUI logPrefab;
+        public ScrollRect scrollRect;
+
+        public void AddLog(string message)
+        {
+            var log = Instantiate(logPrefab, content);
+            log.SetText(message);
+
+            // limit battle log to hold 50 messages max
+            if (content.childCount > 50)
+                Destroy(content.GetChild(0).gameObject);
+
+            Canvas.ForceUpdateCanvases();
+            scrollRect.verticalNormalizedPosition = 0f;
+        }
+    }
+}

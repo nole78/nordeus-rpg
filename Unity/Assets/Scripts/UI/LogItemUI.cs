ea8d4c39bc8e68ee220d82637558b29b4a714513
David Novaković <daki.nole@gmail.com>
2026-05-03 01:22:32 +0200

    Added LogItem prefab

---
+++ Unity/Assets/Scripts/UI/LogItemUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class LogItemUI : MonoBehaviour
+    {
+        public TextMeshProUGUI text;
+
+        public void SetText(string message)
+        {
+            text.text = message;
+        }
+    }
+}
